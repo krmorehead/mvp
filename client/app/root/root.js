a64e4fda6145215fb2152c69ee8d0113083cda0f
Kyle Morehead <krmorehead@gmail.com>
2016-01-04 15:36:32 -0800

    got a hardcoded request to the api working

--- client/app/root/root.js
+++ client/app/root/root.js
@@ -4,12 +4,19 @@ angular.module('hikingApp.root', [])
 	$scope.hello = "hi"
 
 	$scope.trailsRequest = function(){
+		console.log("request started")
+
 		$http({
-    url: "https://trailapi-trailapi.p.mashape.com/?limit=25&q[activities_activity_type_name_eq]=hiking&q[city_cont]=Pleasanton&q[country_cont]=United+States&q[state_cont]=California&radius=50", 
     method: "GET",
+    url: "https://trailapi-trailapi.p.mashape.com/?limit=25&q[activities_activity_type_name_eq]=hiking&q[city_cont]=Pleasanton&q[country_cont]=United+States&q[state_cont]=California&radius=50", 
     headers: {
     	"X-Mashape-Key" : "NMB9t3uVj0mshOMSkYMjILuKeDfip1MD8uYjsnNCCRXwjeridh"
-    },
- });
+    }
+ 		}).success(function(data){
+ 			console.log(data)
+  		console.log("request finished")
+  	})
 	}
 })
+
+//?limit=25&q[activities_activity_type_name_eq]=hiking&q[city_cont]=Pleasanton&q[country_cont]=United+States&q[state_cont]=California&radius=50
