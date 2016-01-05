1d878aa9692f5209ce4ecdd00aa9fcd8864c7f69
Kyle Morehead <krmorehead@gmail.com>
2016-01-04 19:14:49 -0800

    added some styling so the maps will be floating to the right of the descriptions

--- client/app/root/root.js
+++ client/app/root/root.js
@@ -1,33 +1,33 @@
 angular.module('hikingApp.root', [])
 
 .controller("RootController", function($scope, $http){
-	$scope.hello = "hi"
 
+	$scope.keyPress = function(keyCode){
+		if(keyCode === 13){
+			$scope.trailsRequest()
+		}
+	}
+
+
+	$scope.buildMap = function(latitude, longitude){
+
+	}
 
 	$scope.trailsRequest = function(){
 		console.log("starting request")
-		console.log($scope.activity)
-		var query = "https://trailapi-trailapi.p.mashape.com/?" +
-    "limit=25" + "&q" 
+		var query = "https://trailapi-trailapi.p.mashape.com/?" + "limit=25" + "&q" 
     if($scope.activity){
     	query += "[activities_activity_type_name_eq]=" + $scope.activity + "&q"
     }
-    query += "[city_cont]=" + $scope.city + "&q" +
-    "[country_cont]=" + $scope.country + "&q"+
-    "[state_cont]=" + $scope.state + "&" +
-    "radius=" + $scope.milesRadius, 
-
+    query += "[city_cont]=" + $scope.city + "&q" + "[country_cont]=" + $scope.country + "&q"+ "[state_cont]=" + $scope.state + "&" + "radius=" + $scope.milesRadius, 
 
 		$http({
     method: "GET",
     url: query,
     headers: {
     	"X-Mashape-Key" : "NMB9t3uVj0mshOMSkYMjILuKeDfip1MD8uYjsnNCCRXwjeridh"
-    },
-    params: {
-
-    }
- 		}).success(function(data){
+    }})
+    .success(function(data){
  			console.log("request complete")
  			console.log(data)
  			if(data.places.length){
@@ -38,5 +38,10 @@ angular.module('hikingApp.root', [])
   	})
 	}
 })
-
-//?limit=25&q[activities_activity_type_name_eq]=hiking&q[city_cont]=Pleasanton&q[country_cont]=United+States&q[state_cont]=California&radius=50
+// var mapCanvas = document.getElementById('map');
+// var mapOptions = {
+//       center: new google.maps.LatLng(44.5403, -78.5463),
+//       zoom: 8,
+//       mapTypeId: google.maps.MapTypeId.ROADMAP
+//     }
+// var map = new google.maps.Map(mapCanvas, mapOptions);
