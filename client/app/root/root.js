27a876c4c07ad2646343982539ae028b228e38c4
Kyle Morehead <krmorehead@gmail.com>
2016-01-04 16:16:02 -0800

    now displaying names of all trails found

--- client/app/root/root.js
+++ client/app/root/root.js
@@ -5,19 +5,32 @@ angular.module('hikingApp.root', [])
 
 
 	$scope.trailsRequest = function(){
-		console.log("request started")
-		console.log($scope.city, "city")
+		console.log("starting request")
 
 		$http({
     method: "GET",
-    url: "https://trailapi-trailapi.p.mashape.com/?limit=25&q[activities_activity_type_name_eq]=hiking&q[city_cont]=Pleasanton&q[country_cont]=United+States&q[state_cont]=California&radius=50", 
+    url: "https://trailapi-trailapi.p.mashape.com/?" +
+    "limit=25" + "&q" +
+    "[activities_activity_type_name_eq]=hiking" + "&q"+
+    "[city_cont]=" + $scope.city + "&q" +
+    "[country_cont]=" + $scope.country + "&q"+
+    "[state_cont]=" + $scope.state + "&" +
+    "radius=" + $scope.milesRadius, 
     headers: {
     	"X-Mashape-Key" : "NMB9t3uVj0mshOMSkYMjILuKeDfip1MD8uYjsnNCCRXwjeridh"
+    },
+    params: {
+
     }
  		}).success(function(data){
+ 			console.log("request complete")
  			console.log(data)
-  		console.log("request finished")
+			$scope.trails = data.places
   	})
+	}
+
+	var displayTrails = function(data){
+		console.log(data)
 	}
 })
 
